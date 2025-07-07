748eac216641ef0ec8ed4dee13f50b4d6f312c86
Krishna Rajpurohit <73893966+krishield@users.noreply.github.com>
2025-07-07 15:37:35 +0000

    icumed

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,7 +70,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Cyber Security", "Penetration Testing", "Vulnerability Assessment", "Mobile Security", "Web Security"],
+    strings: ["Cloud Security", "Penetration Testing", "Vulnerability Assessment", "Mobile Security", "Web Security", "Cyber Security"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
